527e000f076ae19aa1bc30050bb8c34c18a2aee1
schaumb <schaumb@gmail.com>
2015-03-01 10:59:38 +0100

    really really finish A

--- A/main.cpp
+++ A/main.cpp
@@ -1,39 +1,55 @@
 #include <iostream>
 #include <cstdint>
-#include <vector>
+#include <valarray>
 #include <iterator>
+#include <vector>
+#include <map>
+#include <algorithm>
 
 using maxt = unsigned __int128;
 using data = std::uintmax_t;
 
-std::vector<data> map{0,1,1};
-std::vector<data> sum{0,1,2};
+std::valarray<data> map{0,1,1};
+std::valarray<data> sum{0,1,2};
 
 data mod;
 data c;
-
+data hol = 0;
 data get(data n)
 {
-    if(sum.size() > n)
-        return sum[n];
-
-    ++n;
-    map.reserve(n);
-    sum.reserve(n);
-
-    for(auto it = map.end(), it2 = sum.end(); map.size() != n; ++it, ++it2)
+    while(hol < n)
     {
-        map.insert(it, (static_cast<maxt>(c) * it[-1] + it[-2]) % mod );
-        sum.insert(it2, (static_cast<maxt>(it2[-1]) + *it) % mod );
+        map = map.shift(1);
+        sum = sum.shift(1);
+        map[2] = (static_cast<maxt>(c) * map[1] + map[0]) % mod;
+        sum[2] = (static_cast<maxt>(sum[1]) + map[2]) % mod;
+        ++hol;
     }
-    return sum.back();
+    return sum[0];
 }
 
 int main()
 {
     data t;
     std::cin >> c >> mod >> t;
+
+    std::vector< data > nthTcs(t);
+    std::map< data, data > tCsRes;
     for(data i = 0; i < t ; ++i)
-        std::cout << get( *std::istream_iterator<data>(std::cin) ) << std::endl;
+    {
+        data tcs = *std::istream_iterator<data>(std::cin);
+        nthTcs[i] = tcs;
+        tCsRes[tcs];
+    }
+
+    std::for_each(tCsRes.begin(), tCsRes.end(), [](decltype(*tCsRes.begin())& elem)
+    {
+        elem.second = get(elem.first);
+    });
+
+    for(data i = 0; i < t ; ++i)
+    {
+        std::cout << tCsRes[ nthTcs[i] ] << std::endl;
+    }
 }
 
